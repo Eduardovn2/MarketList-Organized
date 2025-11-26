41830545b7f8b4de559412bcd9c14bac3c4443ef
Eduardovn2 <Eduardovn985@gmail.com>
2025-11-26 12:45:09 -0300

    Feat: adicição da estrutura do projeto organizada entre packages.

---
+++ src/test/java/com/example/MarketList/MarketListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarketList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
